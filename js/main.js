ecee021f18e08cf325d560c01a599c8d0d5be100
ChrisBarrio <ch.barrionuevo89@gmail.com>
2022-02-03 23:23:26 -0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 //swiper
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
+    slidesPerView: 4,
+    spaceBetween: 10,
+    slidesPerGroup: 4,
     loop: true,
     loopFillGroupWithBlank: true,
     pagination: {
@@ -19,3 +19,12 @@ var swiper = new Swiper(".mySwiper", {
 
 
 
+const titulo = document.getElementById("titulo");
+const tituloHover = document.querySelector('#titulo');
+
+tituloHover.addEventListener("click",() =>{
+  console.log("tengo el mouse arriba");
+  titulo.style.color = '#fff';
+  titulo.style.backgroundColor = 'lightblue';
+  titulo.textContent = 'Felicitaciones Campeones!!!';
+})
